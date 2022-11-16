a7d88451b6b6a45632e677d04c204f76cff912d8
jesutofunmi <ogunbonajesutofunmi@gmail.com>
2022-11-16 15:41:12 -0800

    edited file main.c

--- main.c
+++ main.c
@@ -1,56 +1,20 @@
-/*
- * File: main.c
- * Auth: Simon Tagbor
- * Ogunbona Jesutofunmi
- */
-
-/**
- * main - Runs a simple UNIX command interpreter.
- *
- * Return: Always 0.
- */
-
 #include "shell.h"
 
-int main(int __attribute__((__unused__)) argc, char __attribute__((__unused__)) **argv, char *envp[])
+/**
+ * main - Runs the shell
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0
+ */
+int main(int ac, char **av)
 {
-	pid_t child_pid;
-	int status;
-	char *buffarray[] = {NULL, NULL};
-	size_t n;
-	ssize_t read;
-	char *line;
+	shell_t shell;
+	(void) ac;
 
-	line = NULL;
-	while (1)
-	{
-		printf("$ ");
-		n = 0;
-		read = getline(&line, &n, stdin);
-		if (read  == -1)
-		{
-			printf("read failed\n");
-			return (1);
-		}
-		buffarray[0] = strtok(line, " \t\r\n");
+	initialize(&shell, av);
+	signal(SIGINT, handl_signint);
+	repl(&shell);
+	uninitialize(&shell);
 
-		child_pid = fork();
-		if (child_pid == -1)
-		{
-			perror("Error child:");
-			return (1);
-		}
-		if (child_pid == 0)
-		{
-			if (execve(buffarray[0], buffarray, envp) == -1)
-				perror("Error exec gone wrong:");
-		}
-		else
-		{
-			wait(&status);
-		}
-		/* free the memory */
-		free(line);
-	}
-	return (0);
+	return (shell.exitcode);
 }
